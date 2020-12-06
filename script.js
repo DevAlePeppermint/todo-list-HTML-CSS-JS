b8b6aa5a5a656672f713553f535354cc8405c94f
Ale Gropp <xandagropp@hotmail.com>
2020-12-06 20:28:36 -0300

    Adicionado configuração do teclado; fixado bugs do array

--- script.js
+++ script.js
@@ -1,13 +1,51 @@
-let data = [{
-    id:1,
-    title: "estudar HTML"
-}, {
-    id: 2,
-    title: "estudar CSS"
-},{
-    id:3,
-    title: "estudar JS"
+let data = [];
 
-}]
+function renderToDo(){
 
-console.log(data)
+    document.querySelector('.todo').innerHTML = ""
+
+    data.forEach(task => {
+
+        let li = document.createElement('li');
+
+        li.innerHTML = `
+            <input type="checkbox" id="task-${task.id}">
+            <label for="task-${task.id}">${task.title}</label> 
+        `
+
+        //Adicionando um listener para verificar se o input checkbox foi selecionado e mudando a classe dele 
+
+        li.querySelector('input').addEventListener("change", e  => {
+
+        if (e.target.checked ){
+                li.classList.add('complete')
+        } else {
+                li.classList.remove('complete')
+        }
+
+        })
+
+        document.querySelector('.todo').append(li)
+
+    });
+
+}
+
+document.getElementById('new-task').addEventListener('keyup', e => {
+    if (e.key === 'Enter'){
+        console.log(e.target.value)
+
+        data.push({
+            id: data.length +1,
+            title: e.target.value
+        })
+
+        e.target.value= ""
+
+        renderToDo()
+    }
+});
+
+renderToDo();
+
+
